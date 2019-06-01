facbef62daa8f635de2e422a152c85aed59385fb
josemiguelq <jose.miguelq95@gmail.com>
2019-06-01 20:20:03 -0300

    removendo import desnecessario

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text } from 'react-native';
 import translate from "../locale";
 import Utils from '../utils';
 import Styles from '../styles';
 import ShareButton from '../components/ShareButton';
-import share from  '../services/shareText'
 
 export default function Home() {
     const phrases = translate('phrases');

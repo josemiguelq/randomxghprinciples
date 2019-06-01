0d92993987a929f6d916703c450b5134f22e77c7
josemiguelq <jose.miguelq95@gmail.com>
2019-06-01 19:59:10 -0300

    compartilhar frases

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import translate from "../locale";
 import Utils from '../utils';
 import Styles from '../styles';
+import ShareButton from '../components/ShareButton';
+import share from  '../services/shareText'
 
 export default function Home() {
     const phrases = translate('phrases');
+    const phrase = Utils.selectRandomPhrase(phrases)
     return (
         <View style={Styles.container}>
             <View style={Styles.messageBox}>
-                <Text style={Styles.messageBoxTitle}>{Utils.selectRandomPhrase(phrases).title}</Text>
-                <Text style={Styles.messageBoxSubTitle}>{Utils.selectRandomPhrase(phrases).subTitle}</Text>
-                <Text style={Styles.messageBoxBody}>{Utils.selectRandomPhrase(phrases).message}</Text>
+                <Text style={Styles.messageBoxTitle}>{phrase.title}</Text>
+                <Text style={Styles.messageBoxSubTitle}>{phrase.subTitle}</Text>
+                <Text style={Styles.messageBoxBody}>{phrase.message}</Text>
             </View>
+            {/*<Button onPress={() => share(phrase)} title={'Share'}/>*/}
+            <ShareButton toShare={phrase}/>
+
         </View>
     );
 };
--- src/services/shareText.js
+++ src/services/shareText.js
@@ -1,1 +1,14 @@
-const share
+import {Share} from 'react-native';
+
+
+export const share = axioma => {
+    const shareOptions = {
+        title: axioma.title,
+        message: axioma.message,
+        subject: 'Subject'
+    };
+
+    return Share.share(shareOptions);
+};
+
+export default share

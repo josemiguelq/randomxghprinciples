a04ffb024bb50e0f3c67332b2337c6a47c02e302
Sidney Oliveira <oliveira.sidney96@gmail.com>
2019-06-01 19:36:04 -0300

    Apply styles in message box

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,11 @@ export default function Home() {
     const phrases = translate('phrases');
     return (
         <View style={Styles.container}>
-            <Text>{Utils.selectRandomPhrase(phrases).subTitle}</Text>
-            <Text>{Utils.selectRandomPhrase(phrases).message}</Text>
+            <View style={Styles.messageBox}>
+                <Text style={Styles.messageBoxTitle}>{Utils.selectRandomPhrase(phrases).title}</Text>
+                <Text style={Styles.messageBoxSubTitle}>{Utils.selectRandomPhrase(phrases).subTitle}</Text>
+                <Text style={Styles.messageBoxBody}>{Utils.selectRandomPhrase(phrases).message}</Text>
+            </View>
         </View>
     );
 };

e4dbbf6bc461c81e8826af3903098e4a9709cec0
Sidney Oliveira <oliveira.sidney96@gmail.com>
2019-06-01 18:47:13 -0300

    Create home page

--- src/App.js
+++ src/App.js
@@ -1,52 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { View, Text } from 'react-native';
+import translate from "./locale";
+import Utils from './utils';
+import Styles from './styles';
 
-import React, {Component} from 'react';
-import {Button, Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android:
-        'Double tap R on your keyboard to reload,\n' +
-        'Shake or press menu button for dev menu',
-});
-
-const principles = ['Principio 1', 'Principio 2']
-
-type Props = {};
-export default class App extends Component<Props> {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>Principio do dia</Text>
-                {/*<Text style={styles.instructions}>To get started, edit App.js</Text>*/}
-                <Text style={styles.instructions}>{principles[Math.floor(Math.random() * principles.length)]}</Text>
-                <Button title={'Random'}/>
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
+export default function Home() {
+    const phrases = translate('phrases');
+    return (
+        <View style={Styles.container}>
+            <Text>{Utils.selectRandomPhrase(phrases).title}</Text>
+            <Text>{Utils.selectRandomPhrase(phrases).message}</Text>
+        </View>
+    );
+};

0aeded1781807b3e85e5e3936d4da18cea7e43e1
Sidney Oliveira <oliveira.sidney96@gmail.com>
2019-06-01 19:01:07 -0300

    Use navigation to create header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import translate from "./locale";
-import Utils from './utils';
-import Styles from './styles';
+import { createAppContainer, createStackNavigator } from "react-navigation";
+import Home from "./components/Home";
 
-export default function Home() {
-    const phrases = translate('phrases');
-    return (
-        <View style={Styles.container}>
-            <Text>{Utils.selectRandomPhrase(phrases).title}</Text>
-            <Text>{Utils.selectRandomPhrase(phrases).message}</Text>
-        </View>
-    );
-};
+const StackNavigator = createStackNavigator({
+    Home: {
+        screen: Home,
+        navigationOptions: {
+            title: 'Home'
+        }
+    }
+});
+
+export default createAppContainer(StackNavigator)
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import translate from "../locale";
+import Utils from '../utils';
+import Styles from '../styles';
+
+export default function Home() {
+    const phrases = translate('phrases');
+    return (
+        <View style={Styles.container}>
+            <Text>{Utils.selectRandomPhrase(phrases).title}</Text>
+            <Text>{Utils.selectRandomPhrase(phrases).message}</Text>
+        </View>
+    );
+};

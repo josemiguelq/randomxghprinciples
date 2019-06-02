d190fdbff8336d88d7e803395f3ff3119ac163f4
josemiguelq <jose.miguelq95@gmail.com>
2019-06-02 01:18:15 -0300

    traduzindo

--- src/locale/en-US.js
+++ src/locale/en-US.js
@@ -3,114 +3,114 @@ export default {
     randomize : 'Next',
     phrases: [
         {
-            title: 'Axioma 1',
-            subTitle: 'Pensou, não é XGH.',
-            message: 'XGH não pensa, faz a primeira coisa que vem à mente. Não existe segunda opção, a única opção é a mais rápida.'
+            title: 'Axiom 1',
+            subTitle: 'You thought, it\'s not XGH.',
+            message: 'XGH does not think, does the first thing that comes to mind. There is no second option, the only option is the fastest.'
         },
         {
-            title: 'Axioma 2',
+            title: 'Axiom 2',
             subTitle: 'Existem 3 formas de se resolver um problema, a correta, a errada e a XGH, que é igual à errada, só que mais rápida.',
-            message: 'XGH é mais rápido que qualquer metodologia de desenvolvimento de software que você conhece (Vide Axioma 14).'
+            message: 'XGH é mais rápido que qualquer metodologia de desenvolvimento de software que você conhece (Vide Axiom 14).'
         },
         {
-            title: 'Axioma 3',
+            title: 'Axiom 3',
             subTitle: 'Quanto mais XGH você faz, mais precisará fazer.',
             message: 'Para cada problema resolvido usando XGH, mais uns 7 são criados. Mas todos eles serão resolvidos da forma XGH. XGH tende ao infinito.'
         },
         {
-            title: 'Axioma 4',
+            title: 'Axiom 4',
             subTitle: 'XGH é totalmente reativo.',
             message: 'Os erros só existem quando aparecem.'
         },
         {
-            title: 'Axioma 5',
+            title: 'Axiom 5',
             subTitle: 'XGH vale tudo, só não vale dar o toba.',
             message: 'Resolveu o problema? Compilou? Commit e era isso.'
         },
         {
-            title: 'Axioma 6',
+            title: 'Axiom 6',
             subTitle: 'Commit sempre antes de update.',
             message: 'Se der merda, a sua parte estará sempre correta.. e seus colegas que se fodam.'
         },
         {
-            title: 'Axioma 7',
+            title: 'Axiom 7',
             subTitle: 'XGH não tem prazo.',
             message: 'Os prazos passados pelo seu cliente são meros detalhes. Você SEMPRE conseguirá implementar TUDO no tempo necessário (nem que isso implique em acessar o BD por um script malaco).'
         },
         {
-            title: 'Axioma 8',
+            title: 'Axiom 8',
             subTitle: 'Esteja preparado para pular fora quando o barco começar a afundar… ou coloque a culpa em alguém ou algo.',
             message: 'Pra quem usa XGH, um dia o barco afunda. Quanto mais o tempo passa, mais o sistema vira um monstro. O dia que a casa cair, é melhor seu curriculum estar cadastrado na APInfo, ou ter algo pra colocar a culpa.'
         },
         {
-            title: 'Axioma 9',
+            title: 'Axiom 9',
             subTitle: 'Seja autêntico, XGH não respeita padrões.',
             message: 'Escreva o código como você bem entender, se resolver o problema, commit e era isso.'
         },
         {
-            title: 'Axioma 10',
+            title: 'Axiom 10',
             subTitle: 'Não existe refactoring, apenas rework.',
-            message: 'Se der merda, refaça um XGH rápido que solucione o problema. O dia que o rework implicar em reescrever a aplicação toda, pule fora, o barco irá afundar (Vide Axioma 8).'
+            message: 'Se der merda, refaça um XGH rápido que solucione o problema. O dia que o rework implicar em reescrever a aplicação toda, pule fora, o barco irá afundar (Vide Axiom 8).'
         },
         {
-            title: 'Axioma 11',
+            title: 'Axiom 11',
             subTitle: 'XGH é totalmente anárquico.',
-            message: 'A figura de um gerente de projeto é totalmente descartável. Não tem dono, cada um faz o que quiser na hora que os problemas e requisitos vão surgindo (Vide Axioma 4).'
+            message: 'A figura de um gerente de projeto é totalmente descartável. Não tem dono, cada um faz o que quiser na hora que os problemas e requisitos vão surgindo (Vide Axiom 4).'
         },
         {
-            title: 'Axioma 12',
+            title: 'Axiom 12',
             subTitle: 'Se iluda sempre com promessas de melhorias.',
-            message: 'Colocar TODO no código como uma promessa de melhoria ajuda o desenvolvedor XGH a não sentir remorso ou culpa pela cagada que fez. É claro que o refactoring nunca será feito (Vide Axioma 10).'
+            message: 'Colocar TODO no código como uma promessa de melhoria ajuda o desenvolvedor XGH a não sentir remorso ou culpa pela cagada que fez. É claro que o refactoring nunca será feito (Vide Axiom 10).'
         },
         {
-            title: 'Axioma 13',
+            title: 'Axiom 13',
             subTitle: 'XGH é absoluto, não se prende à coisas relativas.',
             message: 'Prazo e custo são absolutos, qualidade é totalmente relativa. Jamais pense na qualidade e sim no menor tempo que a solução será implementada, aliás… não pense, faça!'
         },
         {
-            title: 'Axioma 14',
+            title: 'Axiom 14',
             subTitle: 'XGH é atemporal.',
             message: 'Scrum, XP… tudo isso é modinha. O XGH não se prende às modinhas do momento, isso é coisa de viado. XGH sempre foi e sempre será usado por aqueles que desprezam a qualidade.'
         },
         {
-            title: 'Axioma 15',
+            title: 'Axiom 15',
             subTitle: 'XGH nem sempre é POG.',
-            message: 'Muitas POG’s exigem um raciocínio muito elevado, XGH não raciocina (Vide Axioma 1).'
+            message: 'Muitas POG’s exigem um raciocínio muito elevado, XGH não raciocina (Vide Axiom 1).'
         },
         {
-            title: 'Axioma 16',
+            title: 'Axiom 16',
             subTitle: 'Não tente remar contra a maré.',
             message: 'Caso seus colegas de trabalho usam XGH para programar e você é um coxinha que gosta de fazer as coisas certinhas, esqueça! Pra cada Design Pattern que você usa corretamente, seus colegas gerarão 10 vezes mais código podre usando XGH.'
         },
         {
-            title: 'Axioma 17',
+            title: 'Axiom 17',
             subTitle: 'O XGH não é perigoso até surgir um pouco de ordem.',
-            message: 'Este axioma é muito complexo, mas sugere que o projeto utilizando XGH está em meio ao caos. Não tente por ordem no XGH (Vide Axioma 16), é inútil e você pode jogar um tempo precioso no lixo. Isto fará com que o projeto afunde mais rápido ainda (Vide Axioma 8). Não tente gerenciar o XGH, ele é auto suficiente (Vide Axioma 11), assim como o caos.'
+            message: 'Este axioma é muito complexo, mas sugere que o projeto utilizando XGH está em meio ao caos. Não tente por ordem no XGH (Vide Axiom 16), é inútil e você pode jogar um tempo precioso no lixo. Isto fará com que o projeto afunde mais rápido ainda (Vide Axiom 8). Não tente gerenciar o XGH, ele é auto suficiente (Vide Axiom 11), assim como o caos.'
         },
         {
-            title: 'Axioma 18',
+            title: 'Axiom 18',
             subTitle: 'O XGH é seu brother, mas é vingativo.',
             message: 'Enquanto você quiser, o XGH sempre estará do seu lado. Mas cuidado, não o abandone. Se começar um sistema utilizando XGH e abandoná-lo para utilizar uma metodologia da moda, você estará fudido. O XGH não permite refactoring (vide axioma 10), e seu novo sistema cheio de frescurites entrará em colapso. E nessa hora, somente o XGH poderá salvá-lo.'
         },
         {
-            title: 'Axioma 19',
+            title: 'Axiom 19',
             subTitle: 'Se tiver funcionando, não rela a mão.',
-            message: 'Nunca altere, e muito menos questione um código funcionando. Isso é perda de tempo, mesmo porque refactoring não existe (Vide Axioma 10). Tempo é a engrenagem que move o XGH e qualidade é um detalhe desprezível.'
+            message: 'Nunca altere, e muito menos questione um código funcionando. Isso é perda de tempo, mesmo porque refactoring não existe (Vide Axiom 10). Tempo é a engrenagem que move o XGH e qualidade é um detalhe desprezível.'
         },
         {
-            title: 'Axioma 20',
+            title: 'Axiom 20',
             subTitle: 'Teste é para os fracos.',
             message: 'Se você meteu a mão num sistema XGH, é melhor saber o que está fazendo. E se você sabe o que está fazendo, vai testar pra que? Testes são desperdício de tempo, se o código compilar, é o suficiente.'
         },
         {
-            title: 'Axioma 21',
+            title: 'Axiom 21',
             subTitle: 'Acostume-se ao sentimento de fracasso iminente.',
             message: 'O fracasso e o sucesso andam sempre de mãos dadas, e no XGH não é diferente. As pessoas costumam achar que as chances do projeto fracassar utilizando XGH são sempre maiores do que ele ser bem sucedido. Mas sucesso e fracasso são uma questão de ponto de vista. O projeto foi por água abaixo mas você aprendeu algo? Então pra você foi um sucesso!'
         },
         {
-            title: 'Axioma 22',
+            title: 'Axiom 22',
             subTitle: 'O problema só é seu quando seu nome está no Doc da classe.',
-            message: 'Nunca ponha a mão numa classe cujo autor não é você. Caso um membro da equipe morra ou fique doente por muito tempo, o barco irá afundar! Nesse caso, utilize o Axioma 8.'
+            message: 'Nunca ponha a mão numa classe cujo autor não é você. Caso um membro da equipe morra ou fique doente por muito tempo, o barco irá afundar! Nesse caso, utilize o Axiom 8.'
         },
     ]
 }

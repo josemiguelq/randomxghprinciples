f9f59f4a2921a184b1897ee53f85a6ff372181d0
Sidney Oliveira <oliveira.sidney96@gmail.com>
2019-06-02 01:05:46 -0300

    Add icons in Home tab

--- src/routes/HomeTab.js
+++ src/routes/HomeTab.js
@@ -1,19 +1,26 @@
+import React from 'react';
 import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
 import Home from '../containers/Home';
 import Listing from '../containers/Listing';
 import translate from "../locale";
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
 
 const Routes = createBottomTabNavigator({
     Home: {
         screen: Home,
         navigationOptions: {
-            title: translate('axiomOfTheDay')
+            title: translate('axiomOfTheDay'),
+            tabBarIcon: ({tintColor}) =>
+                <Icon name="lightbulb" size={25} color={tintColor} />
         }
     },
     Listing: {
         screen: Listing,
         navigationOptions: {
-            title: translate('listOfAxims')
+            title: translate('listOfAxims'),
+            tabBarIcon: ({tintColor}) =>
+                <Icon name="list" size={25} color={tintColor} />
         }
     }
 });

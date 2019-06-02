9fd752c278fa2871baf8652bc683df1e1fa4feb9
Sidney Oliveira <oliveira.sidney96@gmail.com>
2019-06-02 03:36:47 -0300

    Add share in utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,22 @@
+import { Share } from 'react-native';
+
 export const selectRandomPhrase = phrases => {
     const phrasesLenght = phrases.length;
     const randomIndex = Math.floor(Math.random() * phrasesLenght);
     return phrases[randomIndex];
 };
 
+export const share = axioma => {
+    const shareOptions = {
+        title: axioma.subTitle,
+        url: '',
+        message: axioma.message,
+        subject: 'Subject',
+    };
+    return Share.share(shareOptions);
+};
+
 export default {
-    selectRandomPhrase
+    selectRandomPhrase,
+    share
 };
